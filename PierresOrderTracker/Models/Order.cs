279fb6d70acfd5c19f345a06d46836c9035fd6e3
Ian Scott <chesnekov@gmail.com>
2020-07-24 13:15:24 -0700

    adds logic for adding unique Id's to order objects. Passes related test

--- PierresOrderTracker/Models/Order.cs
+++ PierresOrderTracker/Models/Order.cs
@@ -8,12 +8,17 @@ namespace PierresOrderTracker.Models
     public string Description { get; set; }
     public int Price { get; set; }
     public string Date { get; set; }
+    public int Id { get; }
+    private static int _nextId = 1;
     public Order (string title, string description, int price, string date)
     {
       Title = title;
       Description = description;
       Price = price;
       Date = date;
+      Id = _nextId;
+      _nextId ++;
+
     }
   }
 }
