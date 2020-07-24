10ad26b5709a086a8447a5ae8de889a2686ba4db
Ian Scott <chesnekov@gmail.com>
2020-07-24 10:18:50 -0700

    adds Order model class file, and an OrderTests file. Updates README.md to reflect progress through specification table

---
+++ PierresOrderTracker.Tests/Models/OrderTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresOrderTracker.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PierresOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests : IDisposable
+  {
+    // public void Dispose()
+    // {
+
+    // }
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      
+    }
+
+  }
+}
