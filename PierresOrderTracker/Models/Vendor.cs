57f5e064dd430323f6e7f24538fe5b38ef5c4e66
Ian Scott <chesnekov@gmail.com>
2020-07-24 09:57:31 -0700

    adds logic to Vendor class which assigns a unique Id to each vendor object, and passes related test. This logic will accomodate future specification of also deleteing Vendor objects, so the same ID will not be reused

--- PierresOrderTracker/Models/Vendor.cs
+++ PierresOrderTracker/Models/Vendor.cs
@@ -4,10 +4,14 @@ namespace PierresOrderTracker.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
+    public int Id { get; set; }
+    private static int _nextId = 1; 
     public Vendor (string name, string description)
     {
       Name = name;
       Description = description;
+      Id =_nextId;
+      _nextId ++;
     }
   }
 }
